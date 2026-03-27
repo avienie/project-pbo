b2d3ae9126abcbfb47f9330a119292bf988eae70
laudy sintya bella <laudysnb@gmail.com>
2026-03-27 20:37:28 +0700

    menambahkan tampilan form

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Input</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<h2>Form Input</h2>
+
+<form method="POST" action="process.php">
+    <label>First Name:</label><br>
+    <input type="text" name="firstname"><br><br>
+
+    <label>Last Name:</label><br>
+    <input type="text" name="lastname"><br><br>
+
+    <label>Phone:</label><br>
+    <input type="text" name="phone"><br><br>
+
+    <label>Address:</label><br>
+    <textarea name="address"></textarea><br><br>
+
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
